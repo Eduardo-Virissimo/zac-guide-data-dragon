d675dc4e483d2c15326a782ae491b49efc844eae
Eduardo <44625373+Eduardo-Virissimo@users.noreply.github.com>
2025-07-24 01:41:41 +0000

    Updates

--- src/components/ChampionInfo.jsx
+++
@@ -1,8 +0,0 @@
-export default function ChampionInfo({ championName, description }) {
-  return (
-    <div>
-      <h2 className="text-xl font-semibold mb-4 text-emerald-400">{championName}</h2>
-      <p className="text-gray-300 leading-relaxed max-w-3xl mx-auto">{description}</p>
-    </div>
-  );
-}
